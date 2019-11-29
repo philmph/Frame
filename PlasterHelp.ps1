8dd8af647bc47bc19cda49e6d229d9ba097c6e41
Philipp Maier <philipp.maier@kapsch.net>
2019-11-29 12:56:00 +0100

    Add help files

--- PlasterHelp.ps1
+++ PlasterHelp.ps1
@@ -6,37 +6,47 @@ Repository:			https://github.com/philmph/Plaster-Template
 
 Filename:			PlasterHelp.ps1
 Creationdate:		26.11.2019
-
-Notes & Usage:
-Call script to generate an empty Plaster manifest
 ############################################################
 #>
 
-param(
-    [string]$Path = "$PSScriptRoot",
+function New-PMPlasterTemplate
+{
+    [CmdletBinding()]
+    
+    param(
+        [string]$Path = $PSScriptRoot,
+    
+        [Parameter(Mandatory)]
+        [ValidatePattern('^\w+$')]
+        [string]$TemplateName,
+    
+        [Parameter(Mandatory)]
+        [string]$Title,
+    
+        [string]$Author = 'Philipp Maier'
+    )
+    
+    begin {
+        Set-StrictMode -Version 3
 
-    [Parameter(Mandatory)]
-    [ValidatePattern('^\w+$')]
-    [string]$TemplateName,
+        Import-Module -Name Plaster -MinimumVersion 1.1.3 -ErrorAction Stop
+    }
+    
+    process {
+        $Directory = New-Item -Path $Path -Name $TemplateName -ItemType Directory -Force
+        $TemplatePath = '{0}\plasterManifest.xml' -f $Directory.FullName
 
-    [Parameter(Mandatory)]
-    [string]$Title,
+        $NewPlasterManifestArgs = @{
+            Path = $TemplatePath
+            TemplateName = $TemplateName
+            TemplateType = 'Project'
+            TemplateVersion = '0.0.1'
+            Title = $Title
+            Author = $Author
+        }
 
-    [string]$Author = 'Philipp Maier'
-)
-
-Import-Module -Name Plaster -MinimumVersion 1.1.3 -ErrorAction Stop
-
-$Directory = New-Item -Path $Path -Name $TemplateName -ItemType Directory -Force
-$TemplatePath = '{0}\{1}.xml' -f $Directory.FullName, $TemplateName
-
-$NewPlasterManifestArgs = @{
-    Path = $TemplatePath
-    TemplateName = $TemplateName
-    TemplateType = 'Project'
-    TemplateVersion = '0.0.1'
-    Title = $Title
-    Author = $Author
+        New-PlasterManifest @NewPlasterManifestArgs
+    }
+    
+    end {}
 }
-
-New-PlasterManifest @NewPlasterManifestArgs
